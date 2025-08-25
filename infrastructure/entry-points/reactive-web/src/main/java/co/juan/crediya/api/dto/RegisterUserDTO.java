303344518f21ef08a326016269b782fdf320bb1f
Juan Ceballos <juandavi789@hotmail.com>
2025-08-24 23:31:10 -0500

    Add request DTO

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/juan/crediya/api/dto/RegisterUserDTO.java
@@ -0,0 +1,34 @@
+package co.juan.crediya.api.dto;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterUserDTO {
+
+    @NotEmpty(message = "Name can't be empty")
+    private String name;
+    @NotEmpty(message = "LastName can't be empty")
+    private String lastName;
+    private Date birthDate;
+    private String address;
+    private String phone;
+    @NotEmpty(message = "Email can't be empty")
+    private String email;
+    @NotNull(message = "baseSalary can't be null")
+    @DecimalMin(value = "0.0", inclusive = true)
+    @DecimalMax(value = "15000000.0", inclusive = true)
+    private BigDecimal baseSalary;
+}
