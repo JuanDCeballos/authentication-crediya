d64c4822cb5bff12d3eaaab1fd3eca5ce2c2a9a1
Juan Ceballos <juandavi789@hotmail.com>
2025-08-24 23:32:36 -0500

    Add Transactional functionality

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/juan/crediya/r2dbc/service/UserService.java
@@ -0,0 +1,30 @@
+package co.juan.crediya.r2dbc.service;
+
+import co.juan.crediya.model.user.User;
+import co.juan.crediya.usecase.user.UserUseCase;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.reactive.TransactionalOperator;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final TransactionalOperator transactionalOperator;
+    private final UserUseCase userUseCase;
+
+    public Mono<User> saveUser(User user) {
+        return userUseCase.saveUser(user)
+                .doOnNext(savedUser -> log.info("User with email {} saved successfully", savedUser.getEmail()))
+                .doOnError(throwable -> log.error("Error saving user: {}", throwable.getMessage()))
+                .single();
+    }
+
+    public Flux<User> getAllUsers() {
+        return userUseCase.getAllUsers();
+    }
+}
