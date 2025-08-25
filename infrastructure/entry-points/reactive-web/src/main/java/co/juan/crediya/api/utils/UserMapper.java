fe20298d7deefccb7a53b28ad9d171ec8c7f40e7
Juan Ceballos <juandavi789@hotmail.com>
2025-08-25 00:22:00 -0500

    Remove unnecessary import

--- infrastructure/entry-points/reactive-web/src/main/java/co/juan/crediya/api/utils/UserMapper.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/juan/crediya/api/utils/UserMapper.java
@@ -3,7 +3,6 @@ package co.juan.crediya.api.utils;
 import co.juan.crediya.api.dto.RegisterUserDTO;
 import co.juan.crediya.model.user.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
