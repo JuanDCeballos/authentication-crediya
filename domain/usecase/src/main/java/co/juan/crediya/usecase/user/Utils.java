f80013b56d6dbd4ccae980f9d5df1b2e58eea2b5
Juan Ceballos <juandavi789@hotmail.com>
2025-08-25 18:02:29 -0500

    Remove unnecessary validation

--- domain/usecase/src/main/java/co/juan/crediya/usecase/user/Utils.java
+++ domain/usecase/src/main/java/co/juan/crediya/usecase/user/Utils.java
@@ -1,29 +1,14 @@
 package co.juan.crediya.usecase.user;
 
 import java.math.BigDecimal;
-import java.util.regex.Pattern;
 
 public class Utils {
 
-    private static final String EMAIL_PATTERN =
-            "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-
-    public static boolean validateField(String field) {
-        return field != null && !field.isBlank();
-    }
-
-    public static boolean validateEmail(String email) {
-        if (!validateField(email)) {
-            return false;
-        }
-
-        return Pattern.compile(EMAIL_PATTERN)
-                .matcher(email)
-                .matches();
-    }
+    private static final BigDecimal MIN = BigDecimal.ZERO;
+    private static final BigDecimal MAX = new BigDecimal("15000000");
 
     public static boolean validateMoney(BigDecimal val) {
-        return validateMoney(BigDecimal.ZERO, new BigDecimal("15000000"), val);
+        return validateMoney(MIN, MAX, val);
     }
 
     public static boolean validateMoney(BigDecimal min, BigDecimal max, BigDecimal value) {
