e477a338aa9eee8930a393b4ad10a87d7349327e
Juan Ceballos <juandavi789@hotmail.com>
2025-08-28 11:02:25 -0500

    Added new ErrorCodes for user_not_found and dni_already_exist

--- domain/model/src/main/java/co/juan/crediya/model/user/exception/ErrorCode.java
+++ domain/model/src/main/java/co/juan/crediya/model/user/exception/ErrorCode.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     EMAIL_ALREADY_EXISTS(409, "Email already exist."),
-    INVALID_BASE_SALARY(422, "The base salary is not within range.");
+    INVALID_BASE_SALARY(422, "The base salary is not within range."),
+    USER_NOT_FOUND(404, "The user with dni {} doesn't exists."),
+    DNI_ALREADY_EXISTS(409, "DNI already exist.");
 
     private final int code;
     private final String message;
