4da2391824c272e1b04fd14ba2f9cbb5e806da31
Juan Ceballos <juandavi789@hotmail.com>
2025-08-29 20:49:21 -0500

    Adjust errorcodes messages

--- domain/model/src/main/java/co/juan/crediya/model/user/exception/ErrorCode.java
+++ domain/model/src/main/java/co/juan/crediya/model/user/exception/ErrorCode.java
@@ -4,16 +4,18 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    EMAIL_ALREADY_EXISTS(409, "Email already exist."),
-    INVALID_BASE_SALARY(422, "The base salary is not within range."),
-    USER_NOT_FOUND(404, "The user with dni {} doesn't exists."),
-    DNI_ALREADY_EXISTS(409, "DNI already exist.");
+    EMAIL_ALREADY_EXISTS("BEC_AE_001", "Email already exist.", 409),
+    INVALID_BASE_SALARY("BEC_SNR", "The base salary is not within range.", 422),
+    USER_NOT_FOUND("BEC_NF", "The user with dni doesn't exists.", 404),
+    DNI_ALREADY_EXISTS("BEC_AE_002", "DNI already exist.", 409);
 
-    private final int code;
+    private final String businessErrorCode;
     private final String message;
+    private final Integer httpCode;
 
-    ErrorCode(int code, String message) {
-        this.code = code;
+    ErrorCode(String businessErrorCode, String message, Integer httpCode) {
+        this.businessErrorCode = businessErrorCode;
         this.message = message;
+        this.httpCode = httpCode;
     }
 }
